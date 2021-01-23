d5fa897e14627a46d8d9860cc632a2d6c4dd167f
JuanPabloRiveraAlvarezINEM <juanpariveraalvarez@gmail.com>
2021-01-22 19:18:23 -0500

    Indicaciones de inicion

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ app.set('port',process.env.PORT || 4500)
 app.use(express.json())
 //rutas
 app.get('/',(req,res)=>{
-  res.send('Hola mundo soy juan pablo desde el pc 2')
+  res.send('<h1>Actividad MySql y Express por Juan Pablo Rivera</h1>')
+  res.send('<p>Para mayo comodidad monitorear las peticiones directamente en la base de datos</p>')
 })
 app.use('/vehiculos', require('./routes/vehiculos'))
 app.use('/tipo_linea', require('./routes/tipo_linea'))
